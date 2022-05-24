fc740ce97dcd24eb6ed41c4420da210d35910244
Nicholas Penree <nick@penree.com>
2022-05-24 14:58:11 -0400

    wip

---
+++ nodes/Puppeteer/Puppeteer.node.options.ts
@@ -0,0 +1,280 @@
+import {
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+/**
+ * Options to be displayed
+ */
+export const nodeDescription: INodeTypeDescription = {
+    displayName: 'Puppeteer',
+    name: 'puppeteer',
+    group: ['puppeteer'],
+    version: 1,
+    description: 'Request a webpage using Puppeteer',
+    defaults: {
+        name: 'Puppeteer',
+        color: '#125580',
+    },
+    icon: 'file:puppeteer.svg',
+    inputs: ['main'],
+    outputs: ['main'],
+    properties: [
+        {
+            displayName: 'URL',
+            name: 'url',
+            type: 'string',
+            required: true,
+            default: '',
+        },
+        {
+            displayName: 'Operation',
+            name: 'operation',
+            type: 'options',
+            options: [
+                {
+                    name: 'Get Page Content',
+                    value: 'getPageContent',
+                    description: 'Gets the full HTML contents of the page',
+                },
+                {
+                    name: 'Get Screenshot',
+                    value: 'getScreenshot',
+                    description: 'Capture all or part of the page as an image',
+                },
+                // {
+                // 	name: 'Get PDF',
+                // 	value: 'getPDF',
+                // 	description: 'Capture all or part of the page as a PDF',
+                // },
+            ],
+            default: 'getPageContent',
+        },
+        {
+            displayName: 'Property Name',
+            name: 'dataPropertyName',
+            type: 'string',
+            required: true,
+            default: 'data',
+            description: 'Name of the binary property in which  to store the image data.',
+            displayOptions: {
+                show: {
+                    operation: [
+                        'getScreenshot',
+                    ],
+                },
+            },
+        },
+        {
+            displayName: 'Type',
+            name: 'imageType',
+            type: 'options',
+            options: [
+                {
+                    name: 'JPEG',
+                    value: 'jpeg',
+                },
+                {
+                    name: 'PNG',
+                    value: 'png',
+                },
+                {
+                    name: 'WebP',
+                    value: 'webp',
+                },
+            ],
+            displayOptions: {
+                show: {
+                    operation: [
+                        'getScreenshot',
+                    ],
+                },
+            },
+            default: 'png',
+            description: 'The image type to use. PNG, JPEG, and WebP are supported.',
+        },
+        {
+            displayName: 'Quality',
+            name: 'quality',
+            type: 'number',
+            typeOptions: {
+                minValue: 0,
+                maxValue: 100,
+            },
+            default: 100,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'getScreenshot',
+                    ],
+                    imageType: [
+                        'jpeg',
+                        'webp',
+                    ],
+                },
+            },
+            description: 'The quality of the image, between 0-100. Not applicable to png images.',
+        },
+        {
+            displayName: 'Full Page',
+            name: 'fullPage',
+            type: 'boolean',
+            required: true,
+            default: false,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'getScreenshot',
+                    ],
+                },
+            },
+            description: 'When true, takes a screenshot of the full scrollable page.',
+        },
+        {
+            displayName: 'Query Parameters',
+            name: 'queryParameters',
+            placeholder: 'Add Parameter',
+            type: 'fixedCollection',
+            typeOptions: {
+                multipleValues: true,
+            },
+            description: 'The query parameter to send.',
+            default: {},
+            options: [
+                {
+                    name: 'parameter',
+                    displayName: 'Parameter',
+                    values: [
+                        {
+                            displayName: 'Name',
+                            name: 'name',
+                            type: 'string',
+                            default: '',
+                            description: 'Name of the parameter.',
+                        },
+                        {
+                            displayName: 'Value',
+                            name: 'value',
+                            type: 'string',
+                            default: '',
+                            description: 'Value of the parameter.',
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            displayName: 'Options',
+            name: 'options',
+            type: 'collection',
+            placeholder: 'Add Option',
+            default: {},
+            options: [
+                {
+                    displayName: 'Emulate Device',
+                    name: 'device',
+                    type: 'options',
+                    default: '',
+                    typeOptions: {
+                        loadOptionsMethod: 'getDevices',
+                    },
+                    required: true,
+                },
+                {
+                    displayName: 'Extra Headers',
+                    name: 'headers',
+                    placeholder: 'Add Header',
+                    type: 'fixedCollection',
+                    typeOptions: {
+                        multipleValues: true,
+                    },
+                    description: 'The headers to send.',
+                    default: {},
+                    options: [
+                        {
+                            name: 'parameter',
+                            displayName: 'Header',
+                            values: [
+                                {
+                                    displayName: 'Name',
+                                    name: 'name',
+                                    type: 'string',
+                                    default: '',
+                                    description: 'Name of the header.',
+                                },
+                                {
+                                    displayName: 'Value',
+                                    name: 'value',
+                                    type: 'string',
+                                    default: '',
+                                    description: 'Value to set for the header.',
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    displayName: 'File Name',
+                    name: 'fileName',
+                    type: 'string',
+                    default: '',
+                    description: 'File name to set in binary data.',
+                },
+                {
+                    displayName: 'Timeout',
+                    name: 'timeout',
+                    type: 'number',
+                    typeOptions: {
+                        minValue: 0,
+                    },
+                    default: 30,
+                    description: 'Maximum navigation time in milliseconds. Pass 0 to disable timeout.',
+                },
+                {
+                    displayName: 'Wait Until',
+                    name: 'waitUntil',
+                    type: 'options',
+                    options: [
+                        {
+                            name: 'load',
+                            value: 'load',
+                            description: 'The load event is fired',
+                        },
+                        {
+                            name: 'DOMContentLoaded',
+                            value: 'DOMContentLoaded',
+                            description: 'The DOMContentLoaded event is fired',
+                        },
+                        {
+                            name: 'networkidle0',
+                            value: 'networkidle0',
+                            description: 'No more than 0 connections for at least 500 ms',
+                        },
+                        {
+                            name: 'networkidle2',
+                            value: 'networkidle2',
+                            description: 'No more than 2 connections for at least 500 ms',
+                        },
+                    ],
+                    default: 'load',
+                    description: 'When to consider navigation succeeded.',
+                },
+                {
+                    displayName: 'Headless mode',
+                    name: 'headless',
+                    type: 'boolean',
+                    required: false,
+                    default: true,
+                    description: 'Whether to run browser in headless mode.',
+                },
+                {
+                    displayName: 'Stealth mode',
+                    name: 'stealthMode',
+                    type: 'boolean',
+                    required: false,
+                    default: false,
+                    description: 'Whether to run browser in headless mode.',
+                },
+            ]
+        },
+    ],
+};
--- nodes/Puppeteer/Puppeteer.node.ts
+++ nodes/Puppeteer/Puppeteer.node.ts
@@ -8,274 +8,25 @@ import {
 	INodePropertyOptions,
 } from 'n8n-workflow';
 
-import * as puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+//import { addExtra } from 'puppeteer-extra';
+import { default as StealthPlugin } from 'puppeteer-extra-plugin-stealth';
+
+import {
+	nodeDescription,
+} from './Puppeteer.node.options';
+
+//const puppeteer = addExtra(vanillaPuppeteer);
 
 const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
 export class Puppeteer implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Puppeteer',
-		name: 'puppeteer',
-		group: ['puppeteer'],
-		version: 1,
-		description: 'Request a webpage using Puppeteer',
-		defaults: {
-			name: 'Puppeteer',
-			color: '#125580',
-		},
-		icon: 'file:puppeteer.svg',
-		inputs: ['main'],
-		outputs: ['main'],
-		properties: [
-			{
-				displayName: 'URL',
-				name: 'url',
-				type: 'string',
-				required: true,
-				default: '',
-			},
-			{
-				displayName: 'Operation',
-				name: 'operation',
-				type: 'options',
-				options: [
-					{
-						name: 'Get Page Content',
-						value: 'getPageContent',
-						description: 'Gets the full HTML contents of the page',
-					},
-					{
-						name: 'Get Screenshot',
-						value: 'getScreenshot',
-						description: 'Capture all or part of the page as an image',
-					},
-				],
-				default: 'getPageContent',
-			},
-			{
-				displayName: 'Property Name',
-				name: 'dataPropertyName',
-				type: 'string',
-				required: true,
-				default: 'data',
-				description: 'Name of the binary property in which  to store the image data.',
-				displayOptions: {
-					show: {
-						operation: [
-							'getScreenshot',
-						],
-					},
-				},
-			},
-			{
-				displayName: 'Type',
-				name: 'imageType',
-				type: 'options',
-				options: [
-					{
-						name: 'JPEG',
-						value: 'jpeg',
-					},
-					{
-						name: 'PNG',
-						value: 'png',
-					},
-					{
-						name: 'WebP',
-						value: 'webp',
-					},
-				],
-				displayOptions: {
-					show: {
-						operation: [
-							'getScreenshot',
-						],
-					},
-				},
-				default: 'png',
-				description: 'The image type to use. PNG, JPEG, and WebP are supported.',
-			},
-			{
-				displayName: 'Quality',
-				name: 'quality',
-				type: 'number',
-				typeOptions: {
-					minValue: 0,
-					maxValue: 100,
-				},
-				default: 100,
-				displayOptions: {
-					show: {
-						operation: [
-							'getScreenshot',
-						],
-						imageType: [
-							'jpeg',
-							'webp',
-						],
-					},
-				},
-				description: 'The quality of the image, between 0-100. Not applicable to png images.',
-			},
-			{
-				displayName: 'Full Page',
-				name: 'fullPage',
-				type: 'boolean',
-				required: true,
-				default: false,
-				displayOptions: {
-					show: {
-						operation: [
-							'getScreenshot',
-						],
-					},
-				},
-				description: 'When true, takes a screenshot of the full scrollable page.',
-			},
-			{
-				displayName: 'Query Parameters',
-				name: 'queryParameters',
-				placeholder: 'Add Parameter',
-				type: 'fixedCollection',
-				typeOptions: {
-					multipleValues: true,
-				},
-				description: 'The query parameter to send.',
-				default: {},
-				options: [
-					{
-						name: 'parameter',
-						displayName: 'Parameter',
-						values: [
-							{
-								displayName: 'Name',
-								name: 'name',
-								type: 'string',
-								default: '',
-								description: 'Name of the parameter.',
-							},
-							{
-								displayName: 'Value',
-								name: 'value',
-								type: 'string',
-								default: '',
-								description: 'Value of the parameter.',
-							},
-						],
-					},
-				],
-			},
-			{
-				displayName: 'Options',
-				name: 'options',
-				type: 'collection',
-				placeholder: 'Add Option',
-				default: {},
-				options: [
-					{
-						displayName: 'Emulate Device',
-						name: 'device',
-						type: 'options',
-						default: '',
-						typeOptions: {
-							loadOptionsMethod: 'getDevices',
-						},
-						required: true,
-					},
-					{
-						displayName: 'Extra Headers',
-						name: 'headers',
-						placeholder: 'Add Header',
-						type: 'fixedCollection',
-						typeOptions: {
-							multipleValues: true,
-						},
-						description: 'The headers to send.',
-						default: {},
-						options: [
-							{
-								name: 'parameter',
-								displayName: 'Header',
-								values: [
-									{
-										displayName: 'Name',
-										name: 'name',
-										type: 'string',
-										default: '',
-										description: 'Name of the header.',
-									},
-									{
-										displayName: 'Value',
-										name: 'value',
-										type: 'string',
-										default: '',
-										description: 'Value to set for the header.',
-									},
-								],
-							},
-						],
-					},
-					{
-						displayName: 'File Name',
-						name: 'fileName',
-						type: 'string',
-						default: '',
-						description: 'File name to set in binary data.',
-					},
-					{
-						displayName: 'Timeout',
-						name: 'timeout',
-						type: 'number',
-						typeOptions: {
-							minValue: 0,
-						},
-						default: 30,
-						description: 'Maximum navigation time in milliseconds. Pass 0 to disable timeout.',
-					},
-					{
-						displayName: 'Wait Until',
-						name: 'waitUntil',
-						type: 'options',
-						options: [
-							{
-								name: 'load',
-								value: 'load',
-								description: 'The load event is fired',
-							},
-							{
-								name: 'DOMContentLoaded',
-								value: 'DOMContentLoaded',
-								description: 'The DOMContentLoaded event is fired',
-							},
-							{
-								name: 'networkidle0',
-								value: 'networkidle0',
-								description: 'No more than 0 connections for at least 500 ms',
-							},
-							{
-								name: 'networkidle2',
-								value: 'networkidle2',
-								description: 'No more than 2 connections for at least 500 ms',
-							},
-						],
-						default: 'load',
-						description: 'When to consider navigation succeeded.',
-					},
-				]
-			},
-		],
-	};
-
+	description: INodeTypeDescription = nodeDescription;
 
 	methods = {
 		loadOptions: {
 			async getDevices(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
 				const deviceNames = Object.keys(puppeteer.devices);
-				const returnData: INodePropertyOptions[] = [
-					// {
-					// 	name: '- Default -',
-					// 	value: '',
-					// },
-				];
+				const returnData: INodePropertyOptions[] = [];
 
 				for (const name of deviceNames) {
 					const device = puppeteer.devices[name];
@@ -295,15 +46,24 @@ export class Puppeteer implements INodeType {
 		let items: INodeExecutionData[] = this.getInputData();
 		let returnData: INodeExecutionData[] = [];
 
-		const browser = await puppeteer.launch({ headless: true });
+		const options = this.getNodeParameter('options', 0, {}) as IDataObject;
 		const operation = this.getNodeParameter('operation', 0) as string;
+		const headless = options.headless !== false;
+		const stealthMode = options.stealthMode === true;
+		const browser = await puppeteer.launch({ headless });
+
+		if (stealthMode) {
+			puppeteer.use(StealthPlugin());
+		}
 
 		for (let itemIndex: number = 0; itemIndex < items.length; itemIndex++) {
-			const options = this.getNodeParameter('options', itemIndex,{}) as IDataObject;
 			const urlString = this.getNodeParameter('url', itemIndex) as string;
 			const { parameter: someHeaders = [] } = (options.headers || {}) as any;
 			const { parameter: queryParameters = [] } = this.getNodeParameter('queryParameters', itemIndex) as any;
-			const requestHeaders = someHeaders.reduce((acc: any, cur: any) => acc[cur.name] = cur.value, {});
+			const requestHeaders = someHeaders.reduce((acc: any, cur: any) => {
+				acc[cur.name] = cur.value;
+				return acc;
+			}, {});
 			const device = options.device as string;
 
 			const url = new URL(urlString);
@@ -333,52 +93,65 @@ export class Puppeteer implements INodeType {
 			const response = await page.goto(url.toString(), { waitUntil, timeout });
 			const headers = await response.headers();
 			const statusCode = response.status();
-			let returnItem;
+			let returnItem: any;
+
 
 			if (statusCode !== 200) {
-				throw new Error(`Request failed with status code ${statusCode}`);
-			}
-
-			if (operation === 'getPageContent') {
-				const body = await page.content();
-				returnItem = {
-					json: {
-						body,
-						headers,
-						statusCode,
-					}
-				};
-			} else if (operation === 'getScreenshot') {
-				const dataPropertyName = this.getNodeParameter('dataPropertyName', itemIndex) as string;
-				const fileName = options.fileName as string;
-				const type = this.getNodeParameter('imageType', itemIndex) as puppeteer.ScreenshotOptions['type'];
-				const fullPage = this.getNodeParameter('fullPage', itemIndex) as boolean;
-				const screenshotOptions: puppeteer.ScreenshotOptions = {
-					type,
-					fullPage,
-				};
-
-				if (type !== 'png') {
-					const quality = this.getNodeParameter('quality', itemIndex) as number;
-					screenshotOptions.quality = quality;
-				}
-
-				if (fileName) {
-					screenshotOptions.path = fileName;
-				}
-
-				const screenshot = await page.screenshot(screenshotOptions) as Buffer;
-				if (screenshot) {
-					const binaryData = await this.helpers.prepareBinaryData(screenshot, screenshotOptions.path, `image/${type}`);
+				if (this.continueOnFail() !== true) {
 					returnItem = {
-						binary: { [dataPropertyName]:binaryData },
 						json: {
 							headers,
 							statusCode,
 						}
-					};
+					}
+					if (operation === 'getPageContent') {
+						returnItem.json.body = await page.content();
+					}
+				} else {
+					throw new Error(`Request failed with status code ${statusCode}`);
 				}
+			} else {
+				if (operation === 'getPageContent') {
+					const body = await page.content();
+					returnItem = {
+						json: {
+							body,
+							headers,
+							statusCode,
+						}
+					};
+				} else if (operation === 'getScreenshot') {
+					const dataPropertyName = this.getNodeParameter('dataPropertyName', itemIndex) as string;
+					const fileName = options.fileName as string;
+					const type = this.getNodeParameter('imageType', itemIndex) as puppeteer.ScreenshotOptions['type'];
+					const fullPage = this.getNodeParameter('fullPage', itemIndex) as boolean;
+					const screenshotOptions: puppeteer.ScreenshotOptions = {
+						type,
+						fullPage,
+					};
 
+					if (type !== 'png') {
+						const quality = this.getNodeParameter('quality', itemIndex) as number;
+						screenshotOptions.quality = quality;
+					}
+
+					if (fileName) {
+						screenshotOptions.path = fileName;
+					}
+
+					const screenshot = await page.screenshot(screenshotOptions) as Buffer;
+					if (screenshot) {
+						const binaryData = await this.helpers.prepareBinaryData(screenshot, screenshotOptions.path, `image/${type}`);
+						returnItem = {
+							binary: { [dataPropertyName]:binaryData },
+							json: {
+								headers,
+								statusCode,
+							}
+						};
+					}
+
+				}
 			}
 
 			await page.close();
